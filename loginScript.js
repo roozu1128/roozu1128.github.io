47e3a075935a4f5baecab2be6eba6f1cbe833bd9
jurirozu <roozu734@gmail.com>
2024-11-03 04:05:38 +0800

    Update loginScript.js

--- loginScript.js
+++ loginScript.js
@@ -11,6 +11,7 @@ document.getElementById("login-form").addEventListener("submit", function (event
 
     // Check if the entered name is in the list of valid usernames
     if (validUsernames.includes(enteredName)) {
+        alert("login succesful!");
         // Here you can redirect to another page if needed
         window.location.href = "main.html"; // Redirect to a welcome page
     } else {
